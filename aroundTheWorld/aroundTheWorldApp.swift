591a034480f2e3e0725a62cbde5e1174bc790c2a
Federico Lupotti <fedelupotti94@gmail.com>
2024-10-16 16:08:48 +0200

    Initial Commit

---
+++ aroundTheWorld/aroundTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  aroundTheWorldApp.swift
+//  aroundTheWorld
+//
+//  Created by Federico Lupotti on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct aroundTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
