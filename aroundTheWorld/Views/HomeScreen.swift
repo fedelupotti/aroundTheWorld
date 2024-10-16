ac6c30aa2d274c15ec5d6f36c46df2caeab4597a
Federico Lupotti <fedelupotti94@gmail.com>
2024-10-16 18:45:07 +0200

    Add basic UI for home screen

---
+++ aroundTheWorld/Views/HomeScreen.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  aroundTheWorld
+//
+//  Created by Federico Lupotti on 16/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var homeViewModel = HomeViewModel(apiService: APIService())
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(homeViewModel.cities) { city in
+                    VStack(alignment: .leading) {
+                        
+                        HStack(alignment: .center) {
+                            Text(city.name ?? "")
+                            Text(city.country ?? "")
+                        }
+                        .font(.title2.bold())
+                        
+                        HStack(alignment: .center) {
+                            Text("Lat: \(city.coordinate?.lat ?? Float())")
+                            Text("Lon: \(city.coordinate?.lon ?? Float())")
+                        }
+                        .font(.callout)
+                        .padding(.top, 2)
+                    }
+                }
+            }
+            .navigationTitle("Arround the Word!")
+            .navigationBarTitleDisplayMode(.large)
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
