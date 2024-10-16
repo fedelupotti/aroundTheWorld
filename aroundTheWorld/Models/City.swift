c72be36280e1b5b93f3a087403d646837eecfdfc
Federico Lupotti <fedelupotti94@gmail.com>
2024-10-16 16:40:30 +0200

    Create city data model

---
+++ aroundTheWorld/Models/City.swift
@@ -0,0 +1,26 @@
+//
+//  City.swift
+//  aroundTheWorld
+//
+//  Created by Federico Lupotti on 16/10/24.
+//
+
+import Foundation
+
+struct City: Decodable {
+    let country: String?
+    let name: String?
+    let id: Int?
+    let coordinate: Coordinate?
+    
+    enum CodingKeys: String, CodingKey {
+        case country, name
+        case id = "_id"
+        case coordinate = "coord"
+    }
+}
+
+struct Coordinate: Decodable {
+    let lon: Float?
+    let lat: Float?
+}
