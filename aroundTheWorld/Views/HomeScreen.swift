2ea1abe7156b4d9519b0ca23b0632c2251303265
Federico Lupotti <fedelupotti94@gmail.com>
2024-10-17 18:39:44 +0200

    Add module methods for views

--- aroundTheWorld/Views/HomeScreen.swift
+++ aroundTheWorld/Views/HomeScreen.swift
@@ -17,47 +17,54 @@ struct ContentView: View {
             .background(Color.white.opacity(0))
     }
     
+    @ViewBuilder
+    func toggleView(for city: City) -> some View {
+        Toggle("", isOn: Binding(
+            get: { city.isFavorite ?? false },
+            set: { newValue in
+                homeViewModel.updateCity(city, isFavorite: newValue)
+            })
+        )
+        .toggleStyle(.city)
+        .padding([.leading, .trailing], 10)
+    }
+    
+    @ViewBuilder
+    func titlesView(for city: City) -> some View {
+        HStack(alignment: .center) {
+            Text(city.name ?? "")
+            Text(city.country ?? "")
+        }
+        .font(.title2.bold())
+    }
+    
+    @ViewBuilder
+    func coordinatesView(for city: City) -> some View {
+        HStack(alignment: .center) {
+            Text("Lat: \(city.coordinate?.lat ?? Double())")
+            Text("Lon: \(city.coordinate?.lon ?? Double())")
+        }
+        .font(.callout)
+        .padding(.top, 2)
+    }
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVStack(alignment: .leading, spacing: 10) {
                     ForEach(homeViewModel.citiesSearched) { city in
-                        
                         NavigationLink {
                             MapScreen(city: city)
                         } label: {
-                            
                             HStack(alignment: .center) {
-                                
-                                Toggle("", isOn: Binding(
-                                    get: { city.isFavorite ?? false },
-                                    set: { newValue in
-                                        homeViewModel.updateCity(city, isFavorite: newValue)
-                                    })
-                                )
-                                .toggleStyle(.city)
-                                .padding([.leading, .trailing], 10)
+                                toggleView(for: city)
                                 
                                 VStack(alignment: .leading) {
-                                    
-                                    HStack(alignment: .center) {
-                                        Text(city.name ?? "")
-                                        Text(city.country ?? "")
-                                    }
-                                    .font(.title2.bold())
-                                    .foregroundStyle(Color.primary)
-                                    
-                                    HStack(alignment: .center) {
-                                        Text("Lat: \(city.coordinate?.lat ?? Double())")
-                                        Text("Lon: \(city.coordinate?.lon ?? Double())")
-                                    }
-                                    .font(.callout)
-                                    .foregroundStyle(Color.primary)
-                                    .padding(.top, 2)
+                                    titlesView(for: city)
+                                    coordinatesView(for: city)
                                 }
+                                .foregroundStyle(Color.primary)
                             }
-                            
-                            
                         }
                     }
                 }
